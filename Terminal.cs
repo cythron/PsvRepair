d2403ccd011c5f0c768c45c46deac2d6ae83a3bd
cythron <63314624+cythron@users.noreply.github.com>
2020-04-16 21:23:46 +0545

    Minor bug fixes

---
+++ Terminal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cytPSV
+{
+    public class Terminal
+    {
+        public class Term
+        {
+            public static void Display(string text)
+            {
+                FrmHome.myConsole.Text = String.Concat(FrmHome.myConsole.Text, text);
+            }
+
+            public static void Reset()
+            {
+                FrmHome.myConsole.Text = "";
+            }
+        }
+    }
+}
