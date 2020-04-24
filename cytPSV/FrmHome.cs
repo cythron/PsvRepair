ed051a27689d7f2b652a10c4e097cf5aec2ac0a8
cythron <comtruise10@gmail.com>
2020-04-24 11:29:46 +0545

    Preparing for Automatic Build

---
+++ cytPSV/FrmHome.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using cytPSV.Pluralsight.Domain;
+using Pluralsight.Domain;
+
+namespace cytPSV
+{
+    public partial class FrmHome : Form
+    {
+        public FrmHome()
+        {
+            InitializeComponent();
+        }
+
+        public static RichTextBox myConsole = new RichTextBox();
+        public static bool isResizeLocked = true;
+        public static bool deleteFileAfterProcessing;
+
+
+        private void FrmHome_Load(object sender, EventArgs e)
+        {
+            deleteFileAfterProcessing = OptDeleteAfterProcessing.Checked;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+
+            console.BackColor = Color.FromArgb(220, 220, 220);
+            myConsole.Size = console.Size;
+            myConsole.Font = console.Font;
+            myConsole.Location = console.Location;
+            myConsole.BackColor = Color.White;
+            myConsole.BorderStyle = console.BorderStyle;
+            myConsole.Cursor = Cursors.Default;
+            myConsole.ForeColor = console.ForeColor;
+            //myConsole.ReadOnly = true;
+            myConsole.AcceptsTab = true;
+            Controls.Add(myConsole);
+            console.Dispose();
+
+            Menu.BackColor = Color.FromArgb(240, 240, 240);
+            //console.BackColor = Color.FromArgb(240, 240, 240);
+            BackColor = Color.FromArgb(40, 40, 40);
+            this.Text = " cytPSV";
+            MainMenu.ForeColor = Color.FromKnownColor(KnownColor.InfoText);
+            MainMenu.AllowDrop = true;
+            this.Focus();
+
+            cytPSV.Terminal.Term.Display("Version 1.1a\n");
+        }
+
+        private void OptDamagedPSV_Click(object sender, EventArgs e)
+        {
+            Video.OpenPsv();
+        }
+        
+        private void OptExit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+
+        private void checkIfResizeLocked(object sender, EventArgs e)
+        {
+            if (isResizeLocked)
+            {
+                Terminal.Term.Display("Resize is Locked");
+                Terminal.Term.Display("\nYou can toggle resizing from the options\n\n");
+            }
+        }
+
+        private void enableLockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isResizeLocked = !isResizeLocked;
+            enableLockToolStripMenuItem.Checked = !enableLockToolStripMenuItem.Checked;
+            MaximizeBox = !MaximizeBox;
+            MaximumSize = new Size(9999,9999);
+        }
+
+        private void OptSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveLog = new SaveFileDialog();
+            if (saveLog.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(saveLog.FileName, FileMode.OpenOrCreate);
+                BinaryWriter writer = new BinaryWriter(fs);
+
+                writer.Write(console.Text);
+                writer.Close();
+            }
+        }
+
+        private void OptDeleteAfterProcessing_Click(object sender, EventArgs e)
+        {
+            OptDeleteAfterProcessing.Checked = !OptDeleteAfterProcessing.Checked;
+            deleteFileAfterProcessing = !deleteFileAfterProcessing;
+        }
+
+        private void clearConsoleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myConsole.Clear();
+        }
+    }
+}
---
+++ cytPSV/VideoEncryptionV2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cytPSV
+{
+    namespace Pluralsight.Domain
+    { 
+        public class VideoEncryptionV2
+        {
+            public static string string1_v2 = "\0¿{U9\u0001®`ë\u0013Ñ[\u001BÏ";
+            public static string string2_v2 = "\u0002\u008D\a\u0099\u0089\u009A%\u0084K°súÁ48äcz@\u009F,í>ö 2\vß\n@*í\vz\u008C\u0004½\u0093\0ÜeË\u0086\u001F\bÖ\u009E ADÓg&ì¶\u0017\u008DÀ\u0014{µìß\u0088Ø\u009FòÕÄ\u0081pªªtC\u008A@\u009C2:Åf\\\\­è\u009Eý\u0002g\u0003|ØBf\u0092 ";
+            private static bool useV1 = true;
+            public static string videoLocation;
+
+            public static void XorBuffer(byte[] buff, int length, long position)
+            {
+                string text = "pluralsight";
+                string text2 = "\u0006?zY¢²\u0085\u009fL¾î0Ö.ì\u0017#©>Å£Q\u0005¤°\u00018Þ^\u008eú\u0019Lqß'\u009d\u0003ßE\u009eM\u0080'x:\0~¹\u0001ÿ 4³õ\u0003Ã§Ê\u000eAË¼\u0090è\u009eî~\u008b\u009aâ\u001b¸UD<\u007fKç*\u001döæ7H\v\u0015Arý*v÷%Âþ¾ä;pü";
+                for (int i = 0; i < length; i++)
+                {
+                    byte b = (byte)((long)(text[(int)((position + (long)i) % (long)text.Length)] ^ text2[(int)((position + (long)i) % (long)text2.Length)]) ^ (position + (long)i) % 251L);
+                    int num = i;
+                    int num2 = num;
+                    buff[num2] ^= b;
+                }
+            }
+
+            public static void XorBufferV2(byte[] buff, int length, long position)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    byte b = (byte)((long)(VideoEncryptionV2.string1_v2[(int)((position + (long)i) % (long)VideoEncryptionV2.string1_v2.Length)] ^ VideoEncryptionV2.string2_v2[(int)((position + (long)i) % (long)VideoEncryptionV2.string2_v2.Length)]) ^ (position + (long)i) % 251L);
+                    int num10 = i;
+                    int num11 = num10;
+                    buff[num11] ^= b;
+                }
+            }
+
+            public static void EncryptBuffer(byte[] buff, int length, long position)
+            {
+                VideoEncryptionV2.XorBufferV2(buff, length, position);
+            }
+
+            public static void DecryptBuffer(byte[] buff, int length, long position)
+            {
+                VideoEncryptionV2.XorBuffer(buff, length, position);
+                if (buff[0] == 0 && buff[1] == 0 && buff[2] == 0)
+                {
+                    VideoEncryptionV2.useV1 = true;
+                    return;
+                }
+                VideoEncryptionV2.XorBuffer(buff, length, position);
+                if (position == 0L && length > 3)
+                {
+                    VideoEncryptionV2.XorBufferV2(buff, length, position);
+                    if (buff[0] == 0 && buff[1] == 0)
+                    {
+                        byte b = buff[2];
+                    }
+                    VideoEncryptionV2.useV1 = false;
+                    return;
+                }
+                if (VideoEncryptionV2.useV1)
+                {
+                    VideoEncryptionV2.XorBuffer(buff, length, position);
+                    return;
+                }
+                VideoEncryptionV2.XorBufferV2(buff, length, position);
+            }
+        }
+    }
+}
